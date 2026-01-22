6d6e65b13026c9185ebf14335fa87954460d87cd
Eldar Abkerimov <eldar.abkerimov.s@gyarab.cz>
2026-01-22 21:48:57 +0100

    Add domecek function to draw a house
    Implement turtle graphics function to draw a house shape.

---
+++ Zelva/domecek.py
@@ -0,0 +1,23 @@
+from turtle import *
+from math import sqrt
+from random import randint
+
+
+
+def domecek():
+    x = randint(50, 350)
+    for _ in range(4):
+        forward(x)
+        left(90)
+
+    left(45)
+    
+    forward(sqrt(2) * x)
+    
+    for _ in range(2):
+        left(90)
+        forward((sqrt(2) * x)/2)
+    
+    left(90)
+    forward(sqrt(2) * x)
+domecek()
