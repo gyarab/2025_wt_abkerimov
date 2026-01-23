c78cb03ac419c9cddd7a1359d5a486172f7a07d8
Eldar Abkerimov <eldar.abkerimov.s@gyarab.cz>
2026-01-23 12:06:15 +0100

    Fetch current EUR/CZK rate and convert currency
    Updated the URL for fetching exchange rates and added logic to extract the EUR rate from the response. Also included user input for converting EUR to CZK.

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,7 +1,23 @@
 import httpx
 
-r = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt?date=21.01.2026')
+r = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt')
 print(r.text)
 print(r.status_code)
 lines = r.text.split('\n')
 print(lines[0])
+
+line_euro = ""
+for line in lines:
+    if "EUR" in line:
+        line_euro = line
+        break
+
+rate_str = line_euro.split('|')[-1].replace(',', '.')
+rate = float(rate_str)
+
+print(f"Current EUR/CZK rate: {rate}")
+
+value_in = float(input("Enter amount in EUR: "))
+
+vlaue_out = value_in * rate
+print(f"Your's {value_in} EUR is {vlaue_out} CZK at the current rate.")
